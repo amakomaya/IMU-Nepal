48d7c296f72c9627e8a3aa9b9e7ccef4ce234f42
RC Poudel <rmchndrapdl@gmail.com>
2020-11-18 14:14:49 +0545

    sidebar manage

--- resources/views/layouts/backend/navigation/list/dho.blade.php
+++ resources/views/layouts/backend/navigation/list/dho.blade.php
@@ -27,7 +27,7 @@
     <a href="#">
         <i class="fa fa-user" aria-hidden="true"></i>
 
-        COVID-19 Cases <span class="fa arrow"></span>
+        ACTIVE CASES <span class="fa arrow"></span>
     </a>
 
     <ul class="nav nav-second-level">
@@ -59,6 +59,30 @@
                 Negative
                 <span class="label label-success pull-right">Negative</span>
 
+            </a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="#">
+        <i class="fa fa-users" aria-hidden="true"></i>
+
+        CLOSED CASES <span class="fa arrow"></span>
+    </a>
+
+    <ul class="nav nav-second-level">
+        <li>
+            <a href="{{ route('cases.recovered.index') }}">
+                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+                Recovered / Discharged
+                <span class="label label-success pull-right">R/D</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('cases.death.index') }}">
+                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+                Deaths
+                <span class="label label-warning pull-right"> Deaths </span>
             </a>
         </li>
     </ul>
