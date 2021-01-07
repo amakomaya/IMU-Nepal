3716eb52f0f94a2ec3a25625361b0cc202a9e2f9
RC Poudel <rmchndrapdl@gmail.com>
2021-01-07 18:41:30 +0545

    rest

--- app/Http/Controllers/External/ExtCaseController.php
+++ app/Http/Controllers/External/ExtCaseController.php
@@ -101,4 +101,87 @@ class ExtCaseController extends Controller
         }
         return ['message' => 'Authentication Failed'];
     }
+
+
+    public function indexV2()
+    {
+        $key = request()->getUser();
+        $secret = request()->getPassword();
+
+        $user = User::where([['username', $key], ['password', md5($secret)], ['role', 'healthworker']])->get()->first();
+
+        if (!empty($user)) {
+            $healthworker = OrganizationMember::where('token', $user->token)->get()->first();
+            $record = SuspectedCase::where('hp_code', $healthworker->hp_code)->active()->with('latestAnc')
+                ->get();
+            $data = collect($record)->map(function ($row) {
+                $response = [];
+
+                $response['token'] = $row->token;
+                $response['name'] = $row->name ?? '';
+                $response['age'] = $row->age ?? '';
+                $response['age_unit'] = $row->age_unit ?? 0;
+                $response['sex'] = $row->sex ?? '';
+                $response['caste'] = $row->caste ?? '';
+                $response['province_id'] = $row->province_id ?? '';
+                $response['district_id'] = $row->district_id ?? '';
+                $response['municipality_id'] = $row->municipality_id ?? '';
+                $response['ward'] = $row->ward ?? '';
+                $response['tole'] = $row->tole ?? '';
+                $response['emergency_contact_one'] = $row->emergency_contact_one ?? '';
+                $response['emergency_contact_two'] = $row->emergency_contact_two ?? '';
+                $response['created_at'] = $row->created_at ?? '';
+
+                $response['result'] = $row->sample_result ?? '';
+                return $response;
+            })->values();
+
+            return response()->json($data);
+        }
+        return ['message' => 'Authentication Failed'];
+    }
+
+    public function storeV2(Request $request)
+    {
+        $key = request()->getUser();
+        $secret = request()->getPassword();
+
+        $user = User::where([['username', $key], ['password', md5($secret)], ['role', 'healthworker']])->get()->first();
+
+        if (!empty($user)) {
+            $healthworker = OrganizationMember::where('token', $user->token)->get()->first();
+            $data = json_decode($request->getContent(), true);
+            foreach ($data as $value) {
+
+                dd($value);
+
+
+
+//                try {
+//                    SuspectedCase::insert([
+//                        'token' =>  'a-'.md5(microtime(true).mt_Rand()),
+//                        'name' => $value['name'],
+//                        'age' => $value['age'],
+//                        'age_unit' => $value['age_unit'],
+//                        'caste' => $value['caste'],
+//                        'sex' => $value['sex'],
+//                        'province_id' => $value['province_id'],
+//                        'district_id' => $value['district_id'],
+//                        'municipality_id' => $value['municipality_id'],
+//                        'ward' => $value['ward'],
+//                        'tole' => $value['tole'],
+//                        'emergency_contact_one' => $value['emergency_contact_one'],
+//                        'emergency_contact_two' => $value['emergency_contact_two'],
+//                        'status' => 1,
+//                        'hp_code' => $healthworker->hp_code,
+//                        'created_by' => $healthworker->token
+//                    ]);
+//                } catch (\Exception $e) {
+//                    return response()->json(['message' => 'Something went wrong, Please try again.']);
+//                }
+            }
+            return response()->json(['message' => 'Data Successfully Sync']);
+        }
+        return ['message' => 'Authentication Failed'];
+    }
 }
