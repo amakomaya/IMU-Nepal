4bb1c9a620e3aa41fb29b277c2138a67134eb082
RC Poudel <rmchndrapdl@gmail.com>
2021-01-27 15:03:18 +0545

    klb records

--- app/Http/Controllers/Backend/VaccinationReportsController.php
+++ app/Http/Controllers/Backend/VaccinationReportsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Helpers\GetHealthpostCodes;
+use App\Models\Organization;
 use App\Models\VaccinationRecord;
 use App\Reports\FilterRequest;
 use App\VialStockDetail;
@@ -16,6 +17,11 @@ class VaccinationReportsController extends Controller
         $hpCodes = GetHealthpostCodes::filter($response);
         $data = array();
         $data['vaccination_count'] = VaccinationRecord::whereIn('hp_code', $hpCodes)->count();
+        if(auth()->user()->role=="main") {
+            $klbValleyHpCodes = Organization::whereIn('district_id', [25, 26, 27])->pluck('hp_code');
+            $data['klb_valley_data'] = VaccinationRecord::whereIn('hp_code', $klbValleyHpCodes)->count();
+        }
+
 //        $data['used'] = VaccinationRecord::whereIn('hp_code', $hpCodes)->count();
 //        $data['stock'] = VialStockDetail::whereIn('hp_code', $hpCodes)->count();
         return view('health-professional.report', ['data' => $data]);
--- resources/views/health-professional/report.blade.php
+++ resources/views/health-professional/report.blade.php
@@ -20,7 +20,25 @@
                     </div>
                 </div>
             </div>
-    </div>
+
+            @if(auth()->user()->role == 'main')
+            <div class="col-lg-12">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa- fa-3x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">{{ $data['klb_valley_data'] }}</div>
+                                <div>Total Kathmandu/Lalitpur/Bhaktapur </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endif
+        </div>
 @endsection
 @section('script')
 
