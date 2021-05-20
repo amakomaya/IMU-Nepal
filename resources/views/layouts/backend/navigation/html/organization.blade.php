c7e7c116ce6d7cdabc553e864e31399c0dee9cfd
Anish Tandukar <anishtandukar@kazistudios.com>
2021-05-20 17:23:11 +0545

    organization sidebar updated

--- resources/views/layouts/backend/navigation/html/organization.blade.php
+++ resources/views/layouts/backend/navigation/html/organization.blade.php
@@ -3,7 +3,7 @@
         <i class="fa fa-hospital-o" aria-hidden="true"></i> Organizations <span class="fa arrow"></span>
     </a>
     <ul class="nav nav-second-level">
-        @if(auth()->user()->role != 'municipality')
+        @if(auth()->user()->role == 'main' || auth()->user()->role == 'center' || auth()->user()->role == 'dho' || auth()->user()->role == 'province')
         <li>
             <a href="{{ route('organization.overview.search') }}" title="Search and Edit Organizations">Search</a>
         </li>
