797faedd951da8f464bd4449a64730c1616716d1
RC Poudel <rmchndrapdl@gmail.com>
2021-05-21 22:59:42 +0545

    mysql dump

--- app/Console/Commands/MySqlDump.php
+++ app/Console/Commands/MySqlDump.php
@@ -1,51 +1,53 @@
 <?php
 
-
 namespace App\Console\Commands;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 use Illuminate\Console\Command;
 
 class MySqlDump extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'db:dump';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Runs the mysqldump utility using info from .env';
+    protected $description = 'Backup the database';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    protected $process;
+
+    public function __construct()
     {
-        $ds = DIRECTORY_SEPARATOR;
+        parent::__construct();
 
-        $host = env('DB_HOST');
-        $username = env('DB_USERNAME');
-        $password = env('DB_PASSWORD');
-        $database = env('DB_DATABASE');
+        $ds = DIRECTORY_SEPARATOR;
 
         $ts = time();
 
         $path = database_path() . $ds . 'backups' . $ds . date('Y', $ts) . $ds . date('m', $ts) . $ds . date('d', $ts) . $ds;
-        $file = date('Y-m-d-His', $ts) . '-dump-' . $database . '.sql';
-        $command = sprintf('mysqldump -h %s -u %s -p\'%s\' %s > %s', $host, $username, $password, $database, $path . $file);
+        $file = date('Y-m-d-His', $ts) . '-dump'.'.sql';
+
+
+        $this->process = new Process(sprintf(
+            'mysqldump -u %s -p%s %s > %s ',
+            config('database.connections.mysql.username'),
+            config('database.connections.mysql.password'),
+            config('database.connections.mysql.database'),
+            $path.$file
+        ));
 
         if (!is_dir($path)) {
             mkdir($path, 0755, true);
         }
+    }
 
-        exec($command);
+    public function handle()
+    {
+//        try {
+            $this->process->mustRun();
+
+//            $this->info('The backup has been proceed successfully.');
+//        } catch (ProcessFailedException $exception) {
+//            $this->error('The backup process has been failed.');
+//        }
     }
 }
