d49386a520842728f2a76f234a1eedc533876868
Anish Tandukar <anishtandukar@kazistudios.com>
2021-05-21 14:39:23 +0545

    healthworker sidebar updated

--- resources/views/layouts/backend/navigation/list/healthworker.blade.php
+++ resources/views/layouts/backend/navigation/list/healthworker.blade.php
@@ -1,13 +1,14 @@
 @can('cases-registration')
     @include('layouts.backend.navigation.html.active-cases')
     @include('layouts.backend.navigation.html.close-cases')
-    @include('layouts.backend.navigation.html.case-report')
 @endcan
 
 @can('lab-received')
     @include('layouts.backend.navigation.html.active-cases-in-lab')
+@endcan
+    
+@can('cases-registration','lab-received')
     @include('layouts.backend.navigation.html.case-report')
-
 @endcan
 
 @can('cases-payment')
