ddc00656d3c75a591c681c619cde62e816ef0c49
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-01 12:44:12 +0545

    datatable js and css files added

--- resources/views/backend/cases/reports/monthly-line-listing.blade.php
+++ resources/views/backend/cases/reports/monthly-line-listing.blade.php
@@ -115,14 +115,6 @@
                                     @if(Auth::user()->role!="healthpost" && Auth::user()->role!="healthworker")
                                         <option value="">All Organization</option>
                                     @endif
-                                    @foreach($healthposts as $healthpost)
-                                        @if($hp_code==$healthpost->hp_code)
-                                            @php($selectedHealthpost = "selected")
-                                        @else
-                                            @php($selectedHealthpost = "")
-                                        @endif
-                                        <option value="{{$healthpost->hp_code}}" {{$selectedHealthpost}}>{{$healthpost->name}}</option>
-                                    @endforeach
                                 </select>
                             </div>
                             <div id ="from_to"></div>
@@ -200,31 +192,31 @@
     <!-- /#page-wrapper -->
 @endsection
 @section('Style')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/datatable/jquery.dataTables.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/datatable/buttons.dataTables.min.css') }}">
 
 @endsection
 @section('script')
-<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.27/build/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#dataTable').DataTable({
-                    responsive: true,
-                    scrollX: true,
-                    pageLength: 50,
-                    dom : 'Bfrtip',
-                    buttons: [
-                        'csv', 'excel', 'pdf', 'print'
-                    ]
-            });
+<script src="{{ asset('js/datatable/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('js/datatable/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('js/datatable/jszip.min.js') }}"></script>
+<script src="{{ asset('js/datatable/pdfmake.min.js') }}"></script>
+<script src="{{ asset('js/datatable/vfs_fonts.js') }}"></script>
+<script src="{{ asset('js/datatable/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('js/datatable/buttons.print.min.js') }}"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#dataTable').DataTable({
+                responsive: true,
+                scrollX: true,
+                pageLength: 50,
+                dom : 'Bfrtip',
+                buttons: [
+                    'csv', 'excel', 'pdf', 'print'
+                ]
         });
-    </script>
+    });
+</script>
 @endsection
 
