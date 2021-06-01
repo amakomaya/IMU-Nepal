371fef2b598fef4ec68ac20c53b40101965c447c
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-01 12:20:32 +0545

    monthly-line-listing datatable export added

--- resources/views/backend/cases/reports/monthly-line-listing.blade.php
+++ resources/views/backend/cases/reports/monthly-line-listing.blade.php
@@ -201,20 +201,26 @@
 
 @endsection
 @section('script')
+<script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.27/build/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
     <script>
-        $('#dataTable').DataTable( {
-            scrollX: true,
-            pageLength: 50,
-            dom :
-                '<"top"f>rt<"button"lp><"clear">',
-
-            buttons: [
-                'copyHtml5',
-                'excelHtml5',
-                'csvHtml5',
-                'pdfHtml5'
-            ]
-        } );
+        $(document).ready(function() {
+            $('#dataTable').DataTable({
+                    responsive: true,
+                    scrollX: true,
+                    pageLength: 50,
+                    dom : 'Bfrtip',
+                    buttons: [
+                        'csv', 'excel', 'pdf', 'print'
+                    ]
+            });
+        });
     </script>
 @endsection
 
