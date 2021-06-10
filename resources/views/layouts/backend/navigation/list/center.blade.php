a38242dfad6057cfac6323d8a0e492e76738772c
RC Poudel <rmchndrapdl@gmail.com>
2021-06-10 13:12:56 +0545

    organization display in center admin

--- resources/views/layouts/backend/navigation/list/center.blade.php
+++ resources/views/layouts/backend/navigation/list/center.blade.php
@@ -2,5 +2,6 @@
 @include('layouts.backend.navigation.html.province')
 @include('layouts.backend.navigation.html.dho')
 @include('layouts.backend.navigation.html.municipality')
+@include('layouts.backend.navigation.html.organization')
 @include('layouts.backend.navigation.html.cases-payment')
 @include('layouts.backend.navigation.html.map')
