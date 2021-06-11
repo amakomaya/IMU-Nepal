c149f9463e48e77be9cc0816d53dadbc064e3e14
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-11 11:24:52 +0545

    public portal popup bug fixed

--- resources/views/public/home/index.blade.php
+++ resources/views/public/home/index.blade.php
@@ -279,40 +279,297 @@
           </div>
         </div>
 
-          <div id="hospital-modal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-              <!-- Modal content-->
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal">&times;</button>
-                  <h4 class="modal-title">Hospital & Isolation</h4>
-                </div>
-                <div class="modal-body">
-                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                    <thead>
-                    <tr>
-                        <th>S.N</th>                                      
-                        <th>Name</th>
-                        <th>Province</th>
-                        <th>District</th>
-                        <th>Municipality</th> 
-                        <th>Address</th>
-                        <th>Phone No:</th> 
-                    </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
+        <div id="hospital-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Hospital & Isolation</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th> 
+                      <th>Address</th>
+                      <th>Phone No:</th> 
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="general-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">General</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Bed Usage</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="hdu-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">HDU</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Bed Usage</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="icu-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">ICU</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Bed Usage</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="ventilator-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Ventilators</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Status</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="oxygen-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Oxygen Facility</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Availability</th>
+                      <th>Daily Usage</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
 
-          
         </div>
+
+        <div id="discharge-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Discharge</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Availability</th>
+                      <th>Total</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div id="admission-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Admission</h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Availability</th>
+                      <th>Total</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div id="death-modal" class="modal fade" role="dialog">
+          <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Death </h4>
+              </div>
+              <div class="modal-body">
+              <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                  <tr>
+                      <th>S.N</th>                                      
+                      <th>Name</th>
+                      <th>Province</th>
+                      <th>District</th>
+                      <th>Municipality</th>
+                      <th>Availability</th>
+                      <th>Total</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+              </table>
+              
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   </div>
