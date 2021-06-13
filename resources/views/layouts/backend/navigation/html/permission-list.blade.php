fbb39fc2ca29a814d1a3f6f4fc219fe2973b2d84
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-13 16:15:37 +0545

    healthworker permissions shown

--- resources/views/layouts/backend/navigation/html/permission-list.blade.php
+++ resources/views/layouts/backend/navigation/html/permission-list.blade.php
@@ -1,17 +1,25 @@
 <div style="position: fixed;
 bottom: 0;
-margin-left: 40px;
-margin-botton: 10px;">
-<li><b><a href="#">Your permissions:</a></b></li>
+margin-left: 15px;
+padding:10px;
+background-color: white;
+width:200px;
+border: 2px solid black">
+<li><b>Your permissions:</b></li>
 
 <?php
-    $permissions = auth()->user()->getAllPermissions()->pluck('name');
+    $permissions = auth()->user()->getAllPermissions()->pluck('name')->sort();
     $permissions = json_decode($permissions);
-?>
+    ?>
 
+<ul>
 @foreach ($permissions as $item)
 <li>
-    <a href="#">{{ $item }}</a>
+    <?php
+        $item = str_replace('-', ' ', $item);
+    ?>
+    {{ ucfirst($item) }}
 </li>
 @endforeach
+</ul>
 </div>
