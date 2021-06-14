22b8ee8543328f880a8a098b6f1ad100d66bafa4
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-14 13:32:09 +0545

    cases result report access for province, organization

--- resources/views/layouts/backend/navigation/list/province.blade.php
+++ resources/views/layouts/backend/navigation/list/province.blade.php
@@ -4,5 +4,7 @@
 @include('layouts.backend.navigation.html.lab-users')
 @include('layouts.backend.navigation.html.active-cases')
 @include('layouts.backend.navigation.html.close-cases')
+@include('layouts.backend.navigation.html.case-result-report')
 @include('layouts.backend.navigation.html.cases-payment')
 @include('layouts.backend.navigation.html.swab')
+
