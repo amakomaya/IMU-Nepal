c4672c91379efa2c99b736435c2b2fb657bd98e6
Anish Tandukar <anishtandukar@kazistudios.com>
2021-06-16 15:48:32 +0545

    api check

--- routes/api.php
+++ routes/api.php
@@ -113,6 +113,18 @@ Route::post('/v1/client', function (Request $request) {
 Route::get('/v1/client', function (Request $request) {
     $hp_code = $request->hp_code;
 
+    // $record = \DB::table('women')
+    //     ->leftJoin('ancs', 'ancs.woman_token', '=', 'women.token')
+    //     ->where('women.hp_code', $hp_code)
+    //     ->where('women.end_case', '0')
+    //     ->select('women.*', 'ancs.result as sample_result')
+    //     ->where('women.created_at', '>=', Carbon::now()->subDays(14)->toDateTimeString())
+    //     ->where(function ($query) {
+    //         $query->where('ancs.result', '!=', '4')
+    //             ->orWhere('women.created_at', '>=', Carbon::now()->subDays(2)->toDateTimeString());
+    //     })
+    //     ->get();
+        
     $record = SuspectedCase::with('ancs')
         ->where('hp_code', $hp_code)
         ->where('end_case', '0')
