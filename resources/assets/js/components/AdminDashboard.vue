1b999e65d1dabf9707aae07c659b1ca0c884b2f5
RC Poudel <rmchndrapdl@gmail.com>
2021-06-18 23:15:19 +0545

    report calculation

--- resources/assets/js/components/AdminDashboard.vue
+++ resources/assets/js/components/AdminDashboard.vue
@@ -527,28 +527,6 @@ export default {
         })
         .finally(() => {
         });
-        this.$swal({
-          title: 'सूचना || सूचना',
-          text: '२० जुन, २०२१ (६ आषाढ २०७८) सम्म IMU-System मा Data-Management को कार्य भइरहेको हुँदा Dashboard र Cases-Overview मा देखिने Data नमिल्न सक्ने जानकारी गराउँदछौ र साथै यस कारण आइपर्ने त्रुटी र समस्याको लागि हामी क्षमा प्राथी छौं |',
-          position: 'top',
-          showClass: {
-            popup: `
-          animate__animated
-          animate__fadeInDown
-          animate__faster
-        `
-          },
-          hideClass: {
-            popup: `
-          animate__animated
-          animate__fadeOutUp
-          animate__faster
-        `
-          },
-          grow: 'row',
-          showConfirmButton: false,
-          showCloseButton: true
-        })
     },
   methods: {
     recordUpdatedAt: function () {
