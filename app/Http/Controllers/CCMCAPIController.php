db24e58074274f01f642bcfe0368565664db7164
anishpro <anishxpro@gmail.com>
2021-08-01 14:19:05 +0545

    ccmc controller updated

--- app/Http/Controllers/CCMCAPIController.php
+++ app/Http/Controllers/CCMCAPIController.php
@@ -20,20 +20,22 @@ class CCMCAPIController extends Controller
             ->selectRaw('age')
             ->selectRaw('date(payment_cases.created_at) as created_at')
             ->selectRaw('CASE
-         WHEN is_death = 1 THEN "Recovered"
+         WHEN is_death = 1 THEN "recovered"
          WHEN is_death = 2 THEN "Death"
          ELSE "active"
         END as case_type')
             ->selectRaw('CASE
-         WHEN gender = 1 THEN "Male"
-         WHEN gender = 2 THEN "Female"
+         WHEN gender = 1 THEN "male"
+         WHEN gender = 2 THEN "female"
          ELSE "other"
         END as gender')
             ->leftJoin('healthposts as h', 'payment_cases.hp_code', 'h.hp_code')
             ->leftJoin('districts as d', 'd.id', 'h.district_id')
-            ->orWhere('is_death', 1)
-            ->orWhere('is_death', 2)
-            ->orWhere('is_death', NULL)
+            ->where(function ($q) {
+                $q->orWhere("is_death", 1)
+                    ->orWhere("is_death", 2)
+                    ->orWhereNull('is_death');
+            })
             ->whereNotNull('d.district_name')
             ->where('payment_cases.created_at', '>=', $startDate)
             ->where('payment_cases.created_at', '<=', $endDate)
@@ -89,7 +91,16 @@ class CCMCAPIController extends Controller
           healthposts.no_of_icu as no_of_icu,
           ifnull(hp_bed_status.icu_occuplied,0) as icu_occupied,
           healthposts.no_of_ventilators as no_of_ventilators,
-          ifnull(hp_bed_status.venilator_occupied,0) as ventilator_occupied')
+          healthposts.hospital_type as hospital_type,
+          healthposts.is_oxygen_facility as is_oxygen_facility,
+          healthposts.phone as phone,
+          ifnull(hp_bed_status.venilator_occupied,0) as ventilator_occupied,
+          provinces.province_name,
+          districts.district_name,
+          municipalities.municipality_name,
+          healthposts.ward_no
+          '
+            )
             ->leftJoin(
                 DB::raw("( select
                   count(IF(health_condition = 1 or health_condition = 2, 1, null)) as occupied_beds,
@@ -98,8 +109,10 @@ class CCMCAPIController extends Controller
                   count(IF(health_condition = 5, 1, null)) as venilator_occupied,
                   hp_code from payment_cases where payment_cases.created_at >=$startDate AND payment_cases.created_at <= $endDate group by hp_code) AS hp_bed_status"), 'hp_bed_status.hp_code', 'healthposts.hp_code'
             )
+            ->leftJoin('provinces', 'provinces.id', 'healthposts.province_id')
+            ->leftJoin('districts', 'districts.id', 'healthposts.district_id')
+            ->leftJoin('municipalities', 'municipalities.id', 'healthposts.municipality_id')
             ->simplePaginate(1000);
         return response(['data' => $data]);
     }
 }
-
