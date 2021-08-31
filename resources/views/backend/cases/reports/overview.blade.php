198128f81f5e7efde47d41a781d2d40e68b915fe
anishpro <anishxpro@gmail.com>
2021-08-31 14:09:12 +0545

    minor changes

--- resources/views/backend/cases/reports/overview.blade.php
+++ resources/views/backend/cases/reports/overview.blade.php
@@ -137,12 +137,12 @@
                                     <th rowspan="2">Province</th>
                                     <th rowspan="2">District</th>
                                     <th rowspan="2">Municipality</th>
-                                    <th colspan="4">Facility</th>
+                                    <th colspan="5">Facility</th>
                                     <th rowspan="2">Total Cost</th>
                                 </tr>
                                 <tr>
-                                    <th>No Symptoms</th>
-                                    <th>General Bed</th>
+                                    <th>General Bed (No Symptoms)</th>
+                                    <th>General Bed (Mild)</th>
                                     <th>HDU</th>
                                     <th>ICU</th>
                                     <th>Ventilator</th>
