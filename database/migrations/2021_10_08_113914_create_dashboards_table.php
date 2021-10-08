8beb1d9185440948e1f5778992a01f78255e1a52
anishpro <anishxpro@gmail.com>
2021-10-08 11:53:58 +0545

    report package created and dashboard table created

---
+++ database/migrations/2021_10_08_113914_create_dashboards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDashboardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dashboards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('province_id');
+            $table->smallInteger('district_id');
+            $table->smallInteger('municipality_id');
+            $table->integer('anitigen_positive');
+            $table->integer('anitigen_negative');
+            $table->integer('pcr_positive');
+            $table->integer('pcr_positive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dashboards');
+    }
+}
